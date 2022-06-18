eb51b6fa3485c7cafd848af9e262e629eac03749
alepha kilo <52881874+abenooo@users.noreply.github.com>
2022-06-18 13:28:21 +0300

    Delete 0x02-python-import_modules directory

--- 0x02-python-import_modules/3-infinite_add.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-if __name__ == "__main__":
-    import sys
-    index = 1
-    sum = 0
-    for index in range(1, len(sys.argv)):
-        sum += int(sys.argv[index])
-    print(sum)
