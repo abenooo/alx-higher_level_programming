2e669546d83528405a80ac8f62a2f17ff906a81b
alepha kilo <52881874+abenooo@users.noreply.github.com>
2022-06-21 11:07:57 +0300

    square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,8 @@
+class Square: 
+  """ this module defines a class Square"""
+  
+  class Square:
+    """ class for Square"""
+    def _init_(self,size):
+       """ Initialize class """
+        self.__size = size
