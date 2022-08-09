026d66f0491a4fa67efc10b5f643fd37181db4e6
alepha kilo <52881874+abenooo@users.noreply.github.com>
2022-08-09 13:47:58 +0300

    fix

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,5 +1,10 @@
--- lists all records of second table where name is not NULL ordered by score
+-- List all records of 'second_table' of db 'hbtn_0c_0'
+-- Don't list rows without a 'name' value
+-- Results should display score and name
+-- Records should be listed descending score
+-- db name will be passed as arg to mysql cmd
+-- Not allowed to use JOIN or UNION
 SELECT score, name
 FROM second_table
-WHERE name IS NOT NULL
-ORDER BY score DESC;v
+HAVING name IS NOT NULL
+ORDER BY score DESC;
